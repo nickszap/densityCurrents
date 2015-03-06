140d04a46a80642afc4b405e7621ddc1b4110c1e
Nick <nickszap@gmail.com>
2015-03-05 21:44:34 -0600

    Threshold approach where a density current should have a minimum buoyancy and pressure perturbation. Lots of unjustified morphology scales and a global threshold to boot

--- driver.py
+++ driver.py
@@ -1,62 +1,36 @@
+#among many resources, http://scipy-lectures.github.io/advanced/image_processing/ seems like a useful introduction to image processing
+# for a list of scipy.ndimage functions: http://docs.scipy.org/doc/scipy/reference/ndimage.html
+
 import numpy as np
 import netCDF4
 import matplotlib.pyplot as plt
 from matplotlib.colors import Normalize
-from scipy import ndimage
+from scipy import ndimage, stats
 
-Cp = 1004.5; Rd = 287.04; Rd_Cp = Rd/Cp; p0 = 1.e5
-grav = 9.81
+Cp = 1004.5; Rd = 287.04; Rd_Cp = Rd/Cp; p0 = 1.e5; grav = 9.81
 
 def calc_potentialTemperature(t,p):
   theta = t*(p0/p)**Rd_Cp
   return theta
 
-def calc_buoyancy(theta):
-  thetaMean = np.mean(theta)
-  buoy = grav*(theta-thetaMean)/thetaMean
-  return buoy
-
-def calc_buoyancy_local(theta):
-  #getting a more targeted idea for the "environmental" thetaMean
-  #nearer to the feature (imagine a global domain -> thetaMean(everywhere) wouldn't mean much)
+def calc_refFields(fieldsIn, windowSpacing=50., gridSpacing=1.):
+  #Input list of 2d fields.
+  #calc regional values as reference "environmental" values, where convolutions define the regional values.
+  #return list of 2d reference fields
   
-  windowSpacing = 50. #square's length
-  gridSpacing = 4.
-  windowLen = int(.5*windowSpacing/gridSpacing)
+  windowLen = int(windowSpacing/gridSpacing)
   
-  ny,nx = theta.shape
-  thetaMean = np.empty((ny,nx),dtype=float)
-  for j in xrange(ny):
-    jMin = max(0,j-windowLen)
-    jMax = min(ny-1,j+windowLen)
-    for i in xrange(nx):
-      iMin = max(0,i-windowLen)
-      iMax = min(nx-1,i+windowLen)
-      #print j,i,jMin,jMax, iMin,iMax
-      thetaMean[j,i] = np.mean(theta[jMin:jMax+1,iMin:iMax+1])
+  wts = np.ones((windowLen,windowLen),dtype=float)
+  countVal = np.ones(fieldsIn[0].shape, dtype=int)
+  nVals = ndimage.filters.convolve(countVal, wts, output=None, mode='reflect')
   
-  buoy = grav*(theta-thetaMean)/thetaMean
-  return buoy
-
-def calc_pertPressure_local(theta):
-  
-  windowSpacing = 50. #square's length
-  gridSpacing = 1.
-  windowLen = int(.5*windowSpacing/gridSpacing)
-  
-  ny,nx = theta.shape
-  thetaMean = np.empty((ny,nx),dtype=float)
-  for j in xrange(ny):
-    jMin = max(0,j-windowLen)
-    jMax = min(ny-1,j+windowLen)
-    for i in xrange(nx):
-      iMin = max(0,i-windowLen)
-      iMax = min(nx-1,i+windowLen)
-      #print j,i,jMin,jMax, iMin,iMax
-      thetaMean[j,i] = np.mean(theta[jMin:jMax+1,iMin:iMax+1])
-  
-  buoy = theta-thetaMean
-  return buoy
+  nFields = len(fieldsIn)
+  refVals = []
+  for iField in xrange(nFields):
+    sumVals = ndimage.filters.convolve(fieldsIn[iField], wts, output=None, mode='reflect')
+    refVals.append(sumVals/nVals)
+    
+  return refVals
 
 def readTimeLevel(data):
   #return t,p,q,u,v from file and convert to useful units
@@ -83,48 +57,23 @@ class MidpointNormalize(Normalize):
         x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
         return np.ma.masked_array(np.interp(value, x, y))
         
-def plot_field_recentered(var, norm, showFig=False):
+def plot_field_recentered(var, norm, title=' ', showFig=False):
   
   plt.figure()  
   plt.pcolormesh(var,norm=norm,cmap=plt.cm.RdBu_r)
   plt.colorbar()
+  plt.title(title)
   
   if (showFig):
     plt.show()
   else:
     return plt
-
-def demo_compareConvolve():
-  #read in data --------------------
-  fDir = '/data01/densityCurrents/'
-  f = fDir+'201407101500.nc'
-  
-  data = netCDF4.Dataset(f,'r')
-  
-  t,p,q,u,v,slp = readTimeLevel(data)
-  ny,nx = t.shape
-  
-  #calculate some variables --------------
-  theta = calc_potentialTemperature(t,p)
-  thetav = theta*(1+.61*q)
-  
-  windowLen = 51
-  wts = np.ones((windowLen,windowLen),dtype=float)
-  countVal = np.ones(t.shape, dtype=int)
-  
-  sumVals = ndimage.filters.convolve(t, wts, output=None, mode='reflect')
-  nVals = ndimage.filters.convolve(countVal, wts, output=None, mode='reflect')
-  meanVal = sumVals/nVals
-  
-  plt.figure()
-  plt.pcolormesh(meanVal)
-  plt.colorbar()
-  plt.show()
   
 def demo():
   #read in data --------------------
   fDir = '/data01/densityCurrents/'
   f = fDir+'201407101500.nc'
+  dxGrid = 1.e3
   
   data = netCDF4.Dataset(f,'r')
   
@@ -135,46 +84,72 @@ def demo():
   theta = calc_potentialTemperature(t,p)
   thetav = theta*(1+.61*q)
   
-  #buoy = calc_buoyancy(thetav); print buoy
-  buoy = calc_buoyancy_local(thetav);
-  gradb_dir = np.gradient(buoy); #print gradb
-  gradb = np.maximum(gradb_dir[0],gradb_dir[1])
+  fieldsRef = calc_refFields([thetav,slp], windowSpacing=50*dxGrid, gridSpacing=dxGrid)
+  thetav_ref = fieldsRef[0]; slp_ref = fieldsRef[1]
   
-  pPerturb = calc_pertPressure_local(slp)
+  buoy = grav*(thetav-thetav_ref)/thetav_ref
+  #gradb_dir = np.gradient(buoy);
+  #gradb = np.maximum(gradb_dir[0],gradb_dir[1])
+  gradb = ndimage.morphological_gradient(buoy, size=(5,5))
   
-  du_dxy = np.gradient(u)
-  dv_dxy = np.gradient(v)
+  pPerturb = slp-slp_ref
+  gradP = ndimage.morphological_gradient(pPerturb, size=(5,5))
+  
+  du_dxy = np.gradient(u/dxGrid)
+  dv_dxy = np.gradient(v/dxGrid)
   div = du_dxy[1]+dv_dxy[0]
+  gradDiv = ndimage.morphological_gradient(div, size=(5,5))
   
   #plot some stuff --------------
   if (False):
     normBuoy = MidpointNormalize(midpoint=0)
-    plot_field_recentered(buoy, normBuoy, showFig=False)
+    plot_field_recentered(buoy, normBuoy, title='buoyancy', showFig=False)
     
     plt.figure()
     plt.pcolormesh(thetav)
     plt.colorbar()
     
     normGrad = MidpointNormalize(midpoint=0)
-    plot_field_recentered(gradb, normGrad, showFig=False)
+    plot_field_recentered(gradb, normGrad, title='grad(buoyancy)',showFig=False)
     
     normP = MidpointNormalize(midpoint=0)
-    plot_field_recentered(pPerturb, normP, showFig=False)
+    plot_field_recentered(pPerturb, normP, title='perturbation pressure',showFig=False)
     
     normDiv = MidpointNormalize(midpoint=0)
-    plot_field_recentered(div, normDiv, showFig=False)
+    plot_field_recentered(div, normDiv,title='divergence', showFig=False)
+    
+    norm = MidpointNormalize(midpoint=0)
+    plot_field_recentered(gradDiv, norm,title='grad(divergence)', showFig=False)
     
     plt.show()
     
-  #hope signal persists across different variables -> correlations
-  corr = gradb*pPerturb
+  #signals should persist across different variables -> correlations
+  signalThresh = (grav*1./300.)*50.
+  signalFlow = -buoy*pPerturb
+  norm = MidpointNormalize(midpoint=0)
+  plot_field_recentered(signalFlow, norm, title='-buoy*pPerturb',showFig=False)
   
-  normCorr = MidpointNormalize(midpoint=0)
-  plot_field_recentered(corr, normCorr, showFig=True)
-
+  if (False):
+    signalFront = gradb*gradP
+    norm = MidpointNormalize(midpoint=0)
+    plot_field_recentered(signalFront, norm, title='gradb*gradp',showFig=False)
+    
+    candidates = (signalFlow>0)*(signalFront>signalThresh)
+  else:
+    candidates = signalFlow>signalThresh
+  
+  candidates = ndimage.morphology.binary_closing(candidates, structure=np.ones((5,5),dtype=int), iterations=1)
+  objs, nObjs = ndimage.measurements.label(candidates)
+  
+  #i think background is always 0?
+  plt.figure()
+  plt.pcolormesh(objs, cmap=plt.cm.flag)
+  plt.colorbar()
+  plt.show()
+  
+  
 if __name__=='__main__':
-  #demo()
-  demo_compareConvolve()
+  demo()
 
 
 
